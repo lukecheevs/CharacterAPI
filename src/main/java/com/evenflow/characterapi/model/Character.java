3f7a1e1da044fb760f1b09d6443485de8e48f66e
lukechee <noemail@gmail.com>
2019-11-09 15:53:01 -0500

    adding id to character

--- src/main/java/com/evenflow/characterapi/model/Character.java
+++ src/main/java/com/evenflow/characterapi/model/Character.java
@@ -1,49 +1,62 @@
 package com.evenflow.characterapi.model;
 
+import java.util.Random;
+
 public abstract class Character {
 
+	private static final Random RANDOM = new Random();
+
+	private int id;
 	private String name;
 	private GenderList gender;
 	private int age;
 	private int height;
 	private int weight;
-	
+
+	public Character() {
+		id = RANDOM.nextInt();
+	}
+
+	public int getId() {
+		return id;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public GenderList getGender() {
 		return gender;
 	}
-	
+
 	public void setGender(GenderList gender) {
 		this.gender = gender;
 	}
-	
+
 	public int getAge() {
 		return age;
 	}
-	
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 	public int getHeight() {
 		return height;
 	}
-	
+
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
+
 	public int getWeight() {
 		return weight;
 	}
-	
+
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
