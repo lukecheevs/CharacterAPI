ff6f365c288bb1e068d3cb4c70872b1c85ca3e22
lukechee <noemail@gmail.com>
2019-11-09 15:48:16 -0500

    getters and setters

--- src/main/java/com/evenflow/characterapi/model/Character.java
+++ src/main/java/com/evenflow/characterapi/model/Character.java
@@ -8,4 +8,43 @@ public abstract class Character {
 	private int height;
 	private int weight;
 	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public GenderList getGender() {
+		return gender;
+	}
+	
+	public void setGender(GenderList gender) {
+		this.gender = gender;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
 }
