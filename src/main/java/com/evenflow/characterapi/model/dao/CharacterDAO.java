13232296b629850357307d3f3104c0b7ae73abef
lukechee <noemail@gmail.com>
2019-11-09 17:01:42 -0500

    character methods in dao

--- src/main/java/com/evenflow/characterapi/model/dao/CharacterDAO.java
+++ src/main/java/com/evenflow/characterapi/model/dao/CharacterDAO.java
@@ -14,7 +14,19 @@ public class CharacterDAO {
 
 	public CharacterDAO() {
 		createCharacters();
+	}
 
+	public AbstractCharacter retrieveCharacterById(int id) {
+		// @formatter:off
+		return characters.stream()
+					.filter(character -> id == character.getId())
+					.findFirst()
+					.orElseThrow(() -> new RuntimeException("Character not found"));
+		// @formatter:on
+	}
+	
+	public void addCharacter(AbstractCharacter character) {
+		characters.add(character);
 	}
 
 	private void createCharacters() {
@@ -22,13 +34,13 @@ public class CharacterDAO {
 		playableCharacter.setName("Alex Canjim");
 		playableCharacter.setAge(23);
 		playableCharacter.setLevel(1);
+		playableCharacter.setHealth(100);
 		playableCharacter.setWeapon("Fists");
 		playableCharacter.setWeight(140);
 		playableCharacter.setHeight(69);
 		playableCharacter.setGender(GenderList.MALE);
-
 		characters.add(playableCharacter);
-		
+
 		Orc orc1 = new Orc();
 		orc1.setName("Jacob Sterlingson");
 		orc1.setAge(28);
@@ -38,9 +50,8 @@ public class CharacterDAO {
 		orc1.setHeight(60);
 		orc1.setWeapon("Hand Axe");
 		orc1.setGender(GenderList.MALE);
-		
 		characters.add(orc1);
-		
+
 		PlayableCharacter playableCharacter2 = new PlayableCharacter();
 		playableCharacter2.setName("Jasmine");
 		playableCharacter2.setAge(34);
@@ -49,8 +60,17 @@ public class CharacterDAO {
 		playableCharacter2.setWeight(120);
 		playableCharacter2.setHeight(63);
 		playableCharacter2.setGender(GenderList.FEMALE);
-		
 		characters.add(playableCharacter2);
-		
+
+		Orc orc2 = new Orc();
+		orc2.setName("Carneluius St Gingamal");
+		orc2.setBoss(true);
+		orc2.setAge(100);
+		orc2.setHealth(200);
+		orc2.setWeapon("Battering Ram");
+		orc2.setWeight(250);
+		orc2.setHeight(72);
+		orc2.setGender(GenderList.MALE);
+		characters.add(orc2);
 	}
 }
